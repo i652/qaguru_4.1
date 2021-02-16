8de72c9e7204865bafa28c9ca5934da4dea12c97
chulietta <36915433+chulietta@users.noreply.github.com>
2021-02-16 19:09:54 +0300

    add google search test

--- src/test/java/YandexSearch.java
+++ src/test/java/YandexSearch.java
@@ -7,9 +7,16 @@ import static com.codeborne.selenide.Selenide.$;
 public class YandexSearch {
 
 @Test
-    public void SelenideSearchYandex() {
+    public void selenideSearchYandex() {
         open("https://yandex.ru/");
         $("#text").setValue("Selenide").pressEnter();
         $(By.linkText("Selenide: concise UI tests in Java")).shouldBe(Condition.appear);
+    }
+    
+@Test
+    public void selenideSearchGoogle() {
+        open("https://google.com/");
+        $(By.name("q")).setValue("Selenide").pressEnter();
+        $(By.linkText("selenide.org")).shouldBe(Condition.appear);
     }
 }
